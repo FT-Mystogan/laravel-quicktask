d8a4cd25b18bdbe138900a08e4d91202e7ab0d74
FT-Mystogan <viponlyc@gmail.com>
2022-02-18 09:18:12 +0700

    creat database

---
+++ database/migrations/2022_02_18_023520_create_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('name', 50);
+            $table->integer('age');
+            $table->string('gender', 10);
+            $table->text('address');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
